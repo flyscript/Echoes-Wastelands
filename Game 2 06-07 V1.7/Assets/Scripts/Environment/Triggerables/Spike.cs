67a12c745fedc1a762b733db629e415e1dfde35a
flyscript <willchap6@gmail.com>
2018-07-10 06:06:21 +0100

    Hazards update
    -Created triggers
    -Created triggerable hazards
    -Consolidated collision detection
    -Refactored other elements

---
+++ Game 2 06-07 V1.7/Assets/Scripts/Environment/Triggerables/Spike.cs
@@ -0,0 +1,59 @@
+﻿/*
+  Pages Items
+  
+  Will Chapman
+  Callum yates
+  05/07/2018
+  
+  Pages is an class that inherits from Item, and sets itself up so that many different pages can be made
+  
+  This is just an example to be built heavily upon by Callum
+  
+*/
+
+//Using
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using JetBrains.Annotations;
+using UnityEngine;
+
+/// <summary>
+/// Falling spike behaviour
+/// </summary>
+public class Spike : Triggerable
+{
+	private GameObject _player;
+	
+	/// <summary>
+	/// Event overriding base Triggerable method is fired by the trigger
+	/// </summary>
+	/// <param name="player"></param>
+    public override void TriggerEvent(GameObject player)
+    {
+	    if (gameObject.GetComponent<Rigidbody2D>() != null)
+	    {
+		    //Sets player for OnTriggerEnter2D
+		    _player = player;
+	    
+		    //Makes spike fall
+		    gameObject.GetComponent<Rigidbody2D>().simulated = true;
+	    }
+    }
+	
+	/// <summary>
+	/// Collision event
+	/// </summary>
+	/// <param name="other">Object collided with</param>
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (_player != null && other != _player && !other.isTrigger)
+		{
+			//If the player's been defined and that's not what we hit, and it's not a trigger, then it must be the ground.
+			Debug.Log("Spike Hit Ground");
+			gameObject.GetComponent<Rigidbody2D>().simulated = false;
+		}
+	}
+	
+}
